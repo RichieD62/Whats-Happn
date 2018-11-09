05218a0b7ab0e9f541547855dcb61c7fe3d90d17
Rich D <richie.donovan62@gmail.com>
2018-11-08 21:22:56 -0600

    Added Seatgeek logo and made some styling changes

--- js/seatgeek.js
+++ js/seatgeek.js
@@ -2,58 +2,58 @@ function submit() {
 
     var modal = $("#myModal2")
     var span = $(".close");
+    var eventName = $("#event-name")
 
     $("#submitBtn").on("click", function (event) {
         event.preventDefault();
 
-        var userInput = $("#event-name").val()
+
         var userZip = $("#zip-code").val()
         var queryURL = "https://api.seatgeek.com/2/events?venue.postal_code=" + userZip + "&client_id=MTM3MzQ5ODJ8MTU0MTAzNTk1NC4z&client_secret=bafaccd7c9def60e73e3d2fcfcca15297124b926e7f941a51303a63dc998c0f3&";
-        
-        if (userInput === null && isNaN(userZip) || userInput === null && userZip.length !== 5) {
-            console.log("nope");
-            $(modal).css("display", "block");
-            $(span).on("click", function(){
-                $(modal).css("display", "none");
-            })
-        } else {
+
+        if (eventName.val().length > 0 || (!isNaN(userZip) && userZip.length === 5)) {
             $.ajax({
                 url: queryURL,
                 method: "GET"
             }).then(function (response) {
                 console.log(response);
-    
-                for (var i=0; i<response.events.length; i++){
-    
+
+                for (var i = 0; i < response.events.length; i++) {
+
                     var event = {
-                        id: response.events[i].id,
                         name: response.events[i].title,
                         dateTime: response.events[i].datetime_local,
                         venue: response.events[i].venue.name,
                         address: response.events[i].venue.address,
                         tickets: response.events[i].url
                     };
-    
+
                     cardGenerator(event)
-                    
+
                 };
             });
-            //html input pattern attribute
+        } else {
+            console.log("nope");
+            $(modal).css("display", "block");
+            $(span).on("click", function () {
+                $(modal).css("display", "none");
+            })
         }
 
-
-       
+        //html input pattern attribute
     })
+
 }
+
 
 function cardGenerator(event) {
     let HTMLTemplate = ''
-    
+
     HTMLTemplate += `
     <div class="col-md-4 mt-4">
-         <div class="card" origin="Seatgeek" eventCode=${event.id} name=${event.name}>
+         <div class="card">
               <div class="card-body">
-                   <img class="img-fluid mb-2" src="https://via.placeholder.com/200"> 
+                   <img class="img-fluid mb-2" src="https://trunited.com/media/catalog/product/s/e/seat_geek_logo_5.jpg"> 
               </div>
               <div class="card-body">
                    <div class="card-text">
@@ -64,7 +64,7 @@ function cardGenerator(event) {
 
                         <a href="${event.tickets}" target="_blank" class="btn btn-primary btn-block mt-4">Get Tickets</a>
 
-                        <button class="favorite">Add To Favorites</a>
+                        <a href=${event.tickets}"target="_blank" class="btn btn-alert alert-danger btn-block mt-4">Add To Favorites</a>
                         
                    </div>
               </div>
@@ -72,7 +72,7 @@ function cardGenerator(event) {
     </div>
 
 `;
-$("#result").prepend(HTMLTemplate)
+    $("#result").prepend(HTMLTemplate)
 }
 
 $(document).ready(submit)
